2188c38bd13c8bbdd34003638c1b8ec2e3365136
miky <mathias89100@gmail.com>
2017-05-06 19:15:25 +0200

    Merge branch 'master' of github.com:mikyprog/SettingsBundle
    # Conflicts:
    #	.gitignore
    #	README.md

---
+++ Menu/AdminMenuBuilder.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: miky
+ * Date: 17/04/17
+ * Time: 17:29
+ */
+
+namespace Miky\Bundle\AdminBundle\Menu;
+
+
+use Knp\Menu\ItemInterface;
+use Miky\Bundle\MenuBundle\Builder\AbstractMenuBuilder;
+use Miky\Bundle\MenuBundle\Event\MenuBuilderEvent;
+
+final class AdminMenuBuilder extends AbstractMenuBuilder
+{
+    const EVENT_NAME = 'miky.menu.admin.main';
+
+    /**
+     * @return ItemInterface
+     */
+    public function createMenu()
+    {
+        $menu = $this->factory->createItem('root');
+
+        $this->eventDispatcher->dispatch(self::EVENT_NAME, new MenuBuilderEvent($this->factory, $menu));
+        $menu
+            ->addChild('directory')
+            ->setLabel('Annuaires')
+            ->setLabelAttribute('icon', 'sitemap')
+        ;
+
+        $menu
+            ->addChild('website')
+            ->setLabel('Sites web')
+            ->setLabelAttribute('icon', 'mouse-pointer')
+        ;
+
+        $menu
+            ->addChild('calendar')
+            ->setLabel('Agenda')
+            ->setLabelAttribute('icon', 'calendar-check-o')
+        ;
+
+        $sub = $menu
+            ->addChild('Revenue')
+            ->setLabel('Revenue')
+            ->setLabelAttribute('icon', 'money')
+        ;
+
+        $subsub = $sub
+            ->addChild('Revenue')
+            ->setLabel('Revenue')
+            ->setLabelAttribute('icon', 'money')
+        ;
+
+        $subsubsub = $subsub
+            ->addChild('Revenue')
+            ->setLabel('Revenue')
+            ->setLabelAttribute('icon', 'money')
+        ;
+
+// @TODO
+//        $schemas = $this->settingsManager->getSchemaRegistry()->all();
+//        $settingsMenu = $menu
+//            ->addChild('settings')
+//            ->setLabel('Paramètres')
+//            ->setLabelAttribute('icon', 'calendar-check-o')
+//        ;
+//        foreach ($schemas as $key => $value){
+//            $settingsMenu
+//                ->addChild("settings_".$key, ['route' => 'adevis_admin_settings_edit', 'routeParameters' => array('schema' => $key)])
+//                ->setLabel("adevis.ui.".$key)
+//                ->setLabelAttribute('icon', 'calendar-check-o')
+//            ;
+//        }
+        return $menu;
+    }
+
+
+}
