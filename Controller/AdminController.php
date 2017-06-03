517129def34132bea0ddc34fdd0ee7caa41943af
miky <mathias89100@gmail.com>
2017-06-04 00:30:19 +0200

    All

--- Controller/AdminController.php
+++ Controller/AdminController.php
@@ -9,8 +9,10 @@
 namespace Miky\Bundle\AdminBundle\Controller;
 
 use Miky\Bundle\ResourceBundle\Controller\ResourceController;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class AdminController extends ResourceController
 {
 // @TODO
+
 }
